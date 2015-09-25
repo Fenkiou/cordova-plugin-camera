49dff0e138de8dad1a7ffd3a73905bbcb3a6cbd6
Alexandre Papin <alexandre.papin@indaclouds.fr>
2015-09-25 22:04:38 +0200

    Forward imagePath argument

--- www/Camera.js
+++ www/Camera.js
@@ -147,9 +147,11 @@ cameraExport.getPicture = function(successCallback, errorCallback, options) {
     var saveToPhotoAlbum = !!options.saveToPhotoAlbum;
     var popoverOptions = getValue(options.popoverOptions, null);
     var cameraDirection = getValue(options.cameraDirection, Camera.Direction.BACK);
+    var imagePath = getValue(options.imagePath);
 
-    var args = [quality, destinationType, sourceType, targetWidth, targetHeight, encodingType,
-                mediaType, allowEdit, correctOrientation, saveToPhotoAlbum, popoverOptions, cameraDirection];
+    var args = [quality, destinationType, sourceType, targetWidth, targetHeight,
+                encodingType, mediaType, allowEdit, correctOrientation,
+                saveToPhotoAlbum, popoverOptions, cameraDirection, imagePath];
 
     exec(successCallback, errorCallback, "Camera", "takePicture", args);
     // XXX: commented out
